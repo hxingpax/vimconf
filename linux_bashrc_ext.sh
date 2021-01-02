035e8b0f8bcd19f1d026ee56b164205458cb9669
xing <thehxpax@gmail.com>
2021-01-03 02:15:18 +0800

    add ext for linux bashrc

---
+++ linux_bashrc_ext.sh
@@ -0,0 +1,21 @@
+alias ga='grep -rnHPI'
+alias cpplint='~/workspace/google/styleguide/cpplint/cpplint.py'
+alias r='rake'
+alias t='tree -L'
+alias ,,='fzf'
+alias f='find'
+
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+
+export ANDROID_HOME=~/Android
+export ANDROID_SDK_ROOT=$ANDROID_HOME/Sdk
+export ANDROID_NDK_ROOT=$ANDROID_HOME/android-ndk-r20
+
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+
+export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
+
+#export GRADLE_OPTS='-DsocksProxyHost=127.0.0.1 -DsocksProxyPort=1080' # Fuck Gradle
+
