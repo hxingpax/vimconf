db9bfdd94cf99a46177a1f5a2a08b80cad905855
xing <thehxpax@gmail.com>
2022-10-11 15:18:04 +0800

    update

--- setup_linux.sh
+++ setup_linux.sh
@@ -11,6 +11,9 @@ alias glog='git log --graph --color'
 
 set -o ignoreeof
 
+# colored GCC warnings and errors
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
 
